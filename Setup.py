09e916249384ef6d50a2ae4db39edb2d66e42c59
ross39 <Rheaney26@gmail.com>
2019-10-09 13:51:21 +0100

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -5,7 +5,16 @@ import keras
 
 train_images = mnist.train_images()
 train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
 
-#To test it's loading properly
-print(train_images.shape) # (60000, 28, 28)
-print(train_labels.shape) # (60000,)
+# Normalize the images.
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+# Reshape the images.
+train_images = np.expand_dims(train_images, axis=3)
+test_images = np.expand_dims(test_images, axis=3)
+
+print(train_images.shape) # (60000, 28, 28, 1)
+print(test_images.shape)  # (10000, 28, 28, 1)
